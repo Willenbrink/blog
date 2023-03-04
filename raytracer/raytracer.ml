8a49e8300b2e14d88e494feca6d2418378779f97
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-03-04 23:19:29 +0100

    RiaW 6

--- raytracer/raytracer.ml
+++ raytracer/raytracer.ml
@@ -24,24 +24,63 @@ module Ray = struct
 end
 type ray = Ray.t = {pos: Vec.t; dir: Vec.t}
 
-type sphere = { center: Vec.t; radius: float; color: Vec.t }
+module Sphere = struct
+  type t = { center: Vec.t; radius: float; color: Vec.t }
 
-let hit_sphere {center; radius; _} {pos; dir} =
-  let pc = pos -| center in
-  let a = Vec.dot dir dir in
-  let b = Vec.dot pc dir *. 2. in
-  let c = Vec.dot pc pc -. radius *. radius in
-  let discr = b*.b -. 4.*.a*.c in
-  discr > 0.
+  let hit {center; radius; _} {pos; dir} t_min t_max =
+    let pc = pos -| center in
+    let a = Vec.mag2 dir in
+    let half_b = Vec.dot pc dir in
+    let c = Vec.mag2 pc -. radius *. radius in
+    let discr = half_b*.half_b -. a*.c in
+    if discr < 0.
+    then None
+    else
+      let sqrtd = sqrt discr in
+      let root1 = ( (-.half_b) -. sqrtd) /. a in
+      let root2 = ( (-.half_b) +. sqrtd) /. a in
+      match t_min < root1 && root1 < t_max, t_min < root2 && root2 < t_max with
+      | true,_ -> Some root1
+      | _, true -> Some root2
+      | _ -> None
 
-let ray_color ({pos; dir} as r) =
-  let sphere = {center = (Vec.make 0. 0. (-1.)); radius = 0.5; color = (Vec.make 1. 0. 0.)} in
-  if hit_sphere sphere r
-  then sphere.color
-  else
+  let hit_sphere {center; radius; _} {pos; dir} =
+    let pc = pos -| center in
+    let a = Vec.dot dir dir in
+    let b = Vec.dot pc dir *. 2. in
+    let c = Vec.dot pc pc -. radius *. radius in
+    let discr = b*.b -. 4.*.a*.c in
+    if discr > 0.
+    then Some (( (-. b) -. sqrt discr) /. (2. *. a))
+    else None
+
+
+end
+
+type sphere = Sphere.t = { center: Vec.t; radius: float; color: Vec.t }
+
+let normal ({center; radius; _}) r dist =
+  Vec.normalize ((Ray.at dist r) -| center)
+
+let hits r world =
+  let f acc el =
+    let max_dist = match acc with
+      | None -> Float.infinity
+      | Some (_,dist) -> dist
+    in
+    match Sphere.hit el r 0. max_dist with
+    | None -> acc
+    | Some hit -> Some (el, hit)
+  in
+  List.fold_left f None world
+
+let color ({pos; dir} as r) = function
+  | None ->
     let unit = Vec.normalize dir in
     let t = 0.5 *. (unit.y +. 1.) in
     Vec.(make 1. 1. 1.) *| (1. -. t) +| Vec.(make 0.5 0.7 1.) *| t
+  | Some (el,dist) ->
+    (normal el r dist +| Vec.(make 1. 1. 1.)) *| 0.5
 
 
 let write_color array row col (color : vec) =
@@ -68,12 +107,27 @@ let main array (w_i,h_i) =
   let horizontal = Vec.make w_vp 0. 0. in
   let vertical = Vec.make 0. h_vp 0. in
   let ll_corner = origin -| horizontal /| 2. -| vertical /| 2. -| (Vec.make 0. 0. focal_length) in
+  let world = [
+    {center = (Vec.make 0. 0. (-1.)); radius = 0.5; color = (Vec.make 1. 0. 0.)};
+    (* {center = (Vec.make 0. c (-1.)); radius = (c -. 0.5); color = (Vec.make 1. 0. 0.)}; *)
+    (* {center = (Vec.make 0. (-10.5) (-1.)); radius = 9.; color = (Vec.make 0. 1. 0.)}; *)
+    {center = (Vec.make 0. (100.5) (-1.)); radius = 100.; color = (Vec.make 0. 1. 0.)};
+
+  ] in
   for row = 0 to h_i - 1 do
     for col = 0 to w_i - 1 do
       let u = (float_of_int col) /. (w -. 1.) in
       let v = (float_of_int row) /. (h -. 1.) in
-      let r = { pos=origin; dir=ll_corner  +| horizontal *| u +| vertical *| v -| origin } in
-      let color = ray_color r in
+      let r = {
+        pos = origin;
+        dir = Vec.normalize @@ ll_corner  +| horizontal *| u +| vertical *| v -| origin
+      }
+      in
+
+      let color =
+        hits r world
+        |> color r
+      in
 
       (* print_chance 0.0001 (fun () -> Printf.printf "%f %f %f\n" r.dir.x r.dir.y r.dir.z); *)
       write_color array row col color
